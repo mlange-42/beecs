9af5622d801e14cc9174da73993fc3a1ad50fa2e
mlange-42 <martin_lange_@gmx.net>
2024-05-03 01:54:09 +0200

    Age cohort groups in reverse order

--- model/sys/age_cohorts.go
+++ model/sys/age_cohorts.go
@@ -20,23 +20,17 @@ func (s *AgeCohorts) Initialize(w *ecs.World) {
 }
 
 func (s *AgeCohorts) Update(w *ecs.World) {
-	newWorkerLarvae := s.eggs.Workers[len(s.eggs.Workers)-1]
-	newDroneLarvae := s.eggs.Drones[len(s.eggs.Drones)-1]
+	shiftCohorts(s.inHive.Workers, s.pupae.Workers[len(s.pupae.Workers)-1])
+	shiftCohorts(s.inHive.Drones, s.pupae.Drones[len(s.pupae.Drones)-1])
+
+	shiftCohorts(s.pupae.Workers, s.larvae.Workers[len(s.larvae.Workers)-1])
+	shiftCohorts(s.pupae.Drones, s.larvae.Drones[len(s.larvae.Drones)-1])
+
+	shiftCohorts(s.larvae.Workers, s.eggs.Workers[len(s.eggs.Workers)-1])
+	shiftCohorts(s.larvae.Drones, s.eggs.Drones[len(s.eggs.Drones)-1])
+
 	shiftCohorts(s.eggs.Workers, 0)
 	shiftCohorts(s.eggs.Drones, 0)
-
-	newWorkerPupae := s.larvae.Workers[len(s.larvae.Workers)-1]
-	newDronePupae := s.larvae.Drones[len(s.larvae.Drones)-1]
-	shiftCohorts(s.larvae.Workers, newWorkerLarvae)
-	shiftCohorts(s.larvae.Drones, newDroneLarvae)
-
-	newWorkers := s.pupae.Workers[len(s.pupae.Workers)-1]
-	newDrones := s.pupae.Drones[len(s.pupae.Drones)-1]
-	shiftCohorts(s.pupae.Workers, newWorkerPupae)
-	shiftCohorts(s.pupae.Drones, newDronePupae)
-
-	shiftCohorts(s.inHive.Workers, newWorkers)
-	shiftCohorts(s.inHive.Drones, newDrones)
 }
 
 func (s *AgeCohorts) Finalize(w *ecs.World) {}

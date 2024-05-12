fe0bc576a1587d66412799ddb16d36ba6161fd43
mlange-42 <martin_lange_@gmx.net>
2024-05-12 20:08:29 +0200

    save plots in SVG format

--- tests/py/plot_quantiles.py
+++ tests/py/plot_quantiles.py
@@ -22,7 +22,7 @@ def plot_quantiles(netlogo_file, beecs_file, out_dir):
 
     for col in columns:
         plot_column(
-            data_beehave, data_beecs, col, quantiles, path.join(out_dir, col + ".png")
+            data_beehave, data_beecs, col, quantiles, path.join(out_dir, col + ".svg")
         )
 
 

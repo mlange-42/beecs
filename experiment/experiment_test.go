3208bd44e39fc50efb495ccb2b8062575abef2c8
mlange-42 <martin_lange_@gmx.net>
2024-05-14 13:24:19 +0200

    fix tests

--- experiment/experiment_test.go
+++ experiment/experiment_test.go
@@ -30,44 +30,44 @@ func TestExperiment(t *testing.T) {
 	assert.Equal(t, 33, e.ParameterSets())
 
 	res := e.Values(0)
-	assert.Equal(t, map[string]any{
-		"a": 0.0,
-		"b": 1,
+	assert.Equal(t, []ParameterValue{
+		{Parameter: "a", Value: 0.0},
+		{Parameter: "b", Value: 1},
 	}, res)
 
 	res = e.Values(5)
-	assert.Equal(t, map[string]any{
-		"a": 0.5,
-		"b": 1,
+	assert.Equal(t, []ParameterValue{
+		{Parameter: "a", Value: 0.5},
+		{Parameter: "b", Value: 1},
 	}, res)
 
 	res = e.Values(10)
-	assert.Equal(t, map[string]any{
-		"a": 1.0,
-		"b": 1,
+	assert.Equal(t, []ParameterValue{
+		{Parameter: "a", Value: 1.0},
+		{Parameter: "b", Value: 1},
 	}, res)
 
 	res = e.Values(11)
-	assert.Equal(t, map[string]any{
-		"a": 0.0,
-		"b": 2,
+	assert.Equal(t, []ParameterValue{
+		{Parameter: "a", Value: 0.0},
+		{Parameter: "b", Value: 2},
 	}, res)
 
 	res = e.Values(12)
-	assert.Equal(t, map[string]any{
-		"a": 0.1,
-		"b": 2,
+	assert.Equal(t, []ParameterValue{
+		{Parameter: "a", Value: 0.1},
+		{Parameter: "b", Value: 2},
 	}, res)
 
 	res = e.Values(32)
-	assert.Equal(t, map[string]any{
-		"a": 1.0,
-		"b": 3,
+	assert.Equal(t, []ParameterValue{
+		{Parameter: "a", Value: 1.0},
+		{Parameter: "b", Value: 3},
 	}, res)
 
 	res = e.Values(33)
-	assert.Equal(t, map[string]any{
-		"a": 0.0,
-		"b": 1,
+	assert.Equal(t, []ParameterValue{
+		{Parameter: "a", Value: 0.0},
+		{Parameter: "b", Value: 1},
 	}, res)
 }

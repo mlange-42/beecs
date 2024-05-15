4362332f57652c5cb7c55bef5ae1a61051d8c4fd
mlange-42 <martin_lange_@gmx.net>
2024-05-15 23:59:01 +0200

    add an example for patch parametrization

---
+++ _examples/patches/main.go
@@ -0,0 +1,83 @@
+// Demonstrates how to parametrize patches.
+package main
+
+import (
+	"github.com/mlange-42/arche-model/reporter"
+	"github.com/mlange-42/beecs/comp"
+	"github.com/mlange-42/beecs/enum/interp"
+	"github.com/mlange-42/beecs/model"
+	"github.com/mlange-42/beecs/obs"
+	"github.com/mlange-42/beecs/params"
+)
+
+func main() {
+	// Get the default parameters.
+	p := params.Default()
+
+	// Change initial patches
+	p.InitialPatches = params.InitialPatches{
+		Patches: []comp.PatchConfig{
+			// A patch with constant resource availability.
+			{
+				DistToColony: 1000,
+				ConstantPatch: &comp.ConstantPatch{
+					Nectar:               20,  // [L]
+					Pollen:               1,   // [kg]
+					NectarConcentration:  1.5, // [mumol/L]
+					DetectionProbability: 0.2,
+				},
+			},
+			// A patch with seasonal resource availability.
+			{
+				DistToColony: 1000,
+				SeasonalPatch: &comp.SeasonalPatch{
+					MaxNectar:            20,  // [L]
+					MaxPollen:            1,   // [kg]
+					NectarConcentration:  1.5, // [mumol/L]
+					DetectionProbability: 0.2,
+
+					SeasonShift: 20, // [d]
+				},
+			},
+			// A patch with scripted resource availability.
+			{
+				DistToColony: 1000,
+				ScriptedPatch: &comp.ScriptedPatch{
+					Nectar: [][2]float64{
+						{0, 0},
+						{100, 20},
+						{250, 0},
+					},
+					Pollen: [][2]float64{
+						{0, 0},
+						{100, 1},
+						{250, 0},
+					},
+					NectarConcentration: [][2]float64{
+						{0, 1.5},
+					},
+					DetectionProbability: [][2]float64{
+						{0, 0.2},
+					},
+					Interpolation: interp.Step,
+				},
+			},
+		},
+	}
+
+	// Create a model with the default sub-models.
+	m := model.Default(&p, nil)
+
+	// Add a CSV outputs for patch nectar and pollen.
+	m.AddSystem(&reporter.CSV{
+		Observer: &obs.PatchNectar{},
+		File:     "out/nectar.csv",
+	})
+	m.AddSystem(&reporter.CSV{
+		Observer: &obs.PatchPollen{},
+		File:     "out/pollen.csv",
+	})
+
+	// Run the model.
+	m.Run()
+}

23b795a1870210bc760c99d00aa3a8b9e914f30d
mlange-42 <martin_lange_@gmx.net>
2024-05-22 21:20:39 +0200

    tweak package docs

---
+++ _tests/doc.go
@@ -0,0 +1,4 @@
+// Validation of the beecs implementation against the original [BEEHAVE] model.
+//
+// [BEEHAVE]: https://beehave-model.net
+package tests
--- comp/doc.go
+++ comp/doc.go
@@ -1,2 +1,5 @@
 // Package comp provides all ECS components required for the beecs model.
+//
+// Components are bundles of closely related state variables of model entities that are represented as ECS entities.
+// In beecs, these entities are forager squadrons and flower patches.
 package comp

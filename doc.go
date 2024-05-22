66eedabbeaec2fcb9cd49faee1f428549dd1b175
mlange-42 <martin_lange_@gmx.net>
2024-05-22 11:55:39 +0200

    rename doc files

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Re-implementation of the [BEEHAVE] model
+// in [Go] using the [Arche] Entity Component System (ECS).
+//
+// # Most important packages:
+//
+//   - [comp] Components
+//
+// [BEEHAVE]: https://beehave-model.net
+// [Go]: https://go.dev
+// [Arche]: https://github.com/mlange-42/arche
+package beecs
---
+++ params/doc.go
@@ -0,0 +1,3 @@
+// Package params provides all parameters for the beecs model,
+// as ECS resources.
+package params

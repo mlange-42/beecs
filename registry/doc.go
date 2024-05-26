3030e3f31f8951b3927ff8262abdef89e1ea6a1d
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2024-05-27 00:30:02 +0200

    Document package `registry` (#73)

---
+++ registry/doc.go
@@ -0,0 +1,4 @@
+// Package registry provides a global registry for custom system, resource and observer types.
+//
+// Registering types allows to retrieve them for model customization, e.g. from JSON.
+package registry
--- registry/registry.go
+++ registry/registry.go
@@ -9,6 +9,7 @@ var observerRegistry = map[string]reflect.Type{}
 var resourcesRegistry = map[string]reflect.Type{}
 var systemsRegistry = map[string]reflect.Type{}
 
+// RegisterObserver registers a type as an observer.
 func RegisterObserver[T any]() {
 	tp := reflect.TypeOf((*T)(nil)).Elem()
 	if _, ok := observerRegistry[tp.String()]; ok {
@@ -17,6 +18,7 @@ func RegisterObserver[T any]() {
 	observerRegistry[tp.String()] = tp
 }
 
+// RegisterResource registers a type as a resource.
 func RegisterResource[T any]() {
 	tp := reflect.TypeOf((*T)(nil)).Elem()
 	if _, ok := resourcesRegistry[tp.String()]; ok {
@@ -25,6 +27,7 @@ func RegisterResource[T any]() {
 	resourcesRegistry[tp.String()] = tp
 }
 
+// RegisterSystem registers a type as a system.
 func RegisterSystem[T any]() {
 	tp := reflect.TypeOf((*T)(nil)).Elem()
 	if _, ok := systemsRegistry[tp.String()]; ok {
@@ -33,16 +36,19 @@ func RegisterSystem[T any]() {
 	systemsRegistry[tp.String()] = tp
 }
 
+// GetObserver gets a registered observer type by type name ([reflect.Type.String]).
 func GetObserver(name string) (reflect.Type, bool) {
 	t, ok := observerRegistry[name]
 	return t, ok
 }
 
+// GetResource gets a registered resource type by type name ([reflect.Type.String]).
 func GetResource(name string) (reflect.Type, bool) {
 	t, ok := resourcesRegistry[name]
 	return t, ok
 }
 
+// GetSystem gets a registered system type by type name ([reflect.Type.String]).
 func GetSystem(name string) (reflect.Type, bool) {
 	t, ok := systemsRegistry[name]
 	return t, ok

cd5d15606c3bcda00d187a3fac63117c13e1cafe
mlange-42 <martin_lange_@gmx.net>
2024-05-27 18:35:07 +0200

    Add observer `Extinction` to report the tick of colony extinction

---
+++ _examples/extinction/main.go
@@ -0,0 +1,42 @@
+// Demonstrates how to measure time to extinction using a callback reporter.
+package main
+
+import (
+	"fmt"
+
+	"github.com/mlange-42/arche-model/reporter"
+	"github.com/mlange-42/beecs/model"
+	"github.com/mlange-42/beecs/obs"
+	"github.com/mlange-42/beecs/params"
+)
+
+func main() {
+	// Get the default parameters.
+	p := params.Default()
+
+	// Run for 100 years, but only until extinction.
+	p.Termination.MaxTicks = 100 * 365
+	p.Termination.OnExtinction = true
+
+	// Increase handling time to provoke extinction.
+	p.HandlingTime.NectarGathering = 3600
+
+	// Create a model with the default sub-models.
+	m := model.Default(&p, nil)
+
+	// Add a CSV output system using observer [obs.WorkerCohorts].
+	m.AddSystem(&reporter.RowCallback{
+		Observer: &obs.Extinction{},
+		Callback: func(step int, row []float64) {
+			if row[0] > 0 {
+				fmt.Printf("Extinction on tick %d (%02f years)\n", int(row[1]), row[1]/365)
+			} else {
+				fmt.Println("No extinction")
+			}
+		},
+		Final: true,
+	})
+
+	// Run the model.
+	m.Run()
+}
---
+++ obs/extinction.go
@@ -0,0 +1,35 @@
+package obs
+
+import (
+	"github.com/mlange-42/arche-model/resource"
+	"github.com/mlange-42/arche/ecs"
+	"github.com/mlange-42/beecs/globals"
+)
+
+// Extinction is a row observer for the tick of extinction of all bees.
+//
+// Best use as a finalization observer.
+// Columns are "Extinct" and "ExtinctionTick".
+type Extinction struct {
+	time *resource.Tick
+	pop  *globals.PopulationStats
+	data []float64
+}
+
+func (o *Extinction) Initialize(w *ecs.World) {
+	o.time = ecs.GetResource[resource.Tick](w)
+	o.pop = ecs.GetResource[globals.PopulationStats](w)
+	o.data = make([]float64, len(o.Header()))
+}
+func (o *Extinction) Update(w *ecs.World) {
+	if o.data[0] == 0 && o.pop.TotalPopulation == 0 {
+		o.data[0] = 1
+		o.data[1] = float64(o.time.Tick)
+	}
+}
+func (o *Extinction) Header() []string {
+	return []string{"Extinct", "ExtinctionTick"}
+}
+func (o *Extinction) Values(w *ecs.World) []float64 {
+	return o.data
+}
